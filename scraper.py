991ffbbcaa721432b1e57621df5c6e79dcc21f3a
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-07-10 13:44:23 -0400

    updates

--- scraper.py
+++ scraper.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Gujarat, Ahmedabad, Maninagar
+import sys
+import time
+
 import re
 import string
 import urlparse
@@ -27,26 +31,81 @@ class CPCBScraper(object):
 
         # Iterate through each state
         for state_index in state_option_indexes:
+            state_select = Select(self.driver.find_element_by_id('ddlState'))
+
+            #
+            # Selecting the state will cause the progress div to get reloaded
+            # so we must record this before we select the state
+            #
+            old_progress_div = self.driver.find_element_by_id('UpdateProgress1')
             state_select.select_by_index(state_index)
 
+            print 'State', state_select.options[state_index].text
+
+            if state_select.options[state_index].text == 'Delhi':
+                print 'break'
+
             def done_loading(driver):
+                time.sleep(1)
                 div = driver.find_element_by_id('UpdateProgress1')
-                return div.is_displayed() == False            
+
+                print 'old_progress_div', old_progress_div
+                print 'new_progress_div', div
+
+                return div != old_progress_div
 
             # Wait until city loaded
             wait = WebDriverWait(self.driver, 10)
             wait.until(done_loading)
 
+            print 'now get city'
+
             city_select = Select(self.driver.find_element_by_id('ddlCity'))
             city_option_indexes = range(1, len(city_select.options))            
 
             for city_index in city_option_indexes:
+                old_progress_div = self.driver.find_element_by_id('UpdateProgress1')
                 city_select.select_by_index(city_index)
+
+                print 'City', city_select.options[city_index].text
 
                 # Wait until station loaded
                 wait = WebDriverWait(self.driver, 10)
                 wait.until(done_loading)
 
+                station_select = Select(self.driver.find_element_by_id('ddlStation'))
+                station_option_indexes = range(1, len(station_select.options))            
+
+                for station_index in station_option_indexes:
+                    old_progress_div = self.driver.find_element_by_id('UpdateProgress1')
+                    station_select.select_by_index(station_index)
+
+                    print 'Station', station_select.options[station_index].text
+
+                    # Wait until parameters loaded
+                    wait = WebDriverWait(self.driver, 10)
+                    wait.until(done_loading)
+
+                    # Select All and then click Add
+                    param_select = Select(self.driver.find_element_by_id('lstBoxChannelLeft'))                    
+                    param_select.select_by_value('All')
+                    
+                    old_progress_div = self.driver.find_element_by_id('UpdateProgress1')
+
+                    add = self.driver.find_element_by_id('btnAdd')
+                    add.click()
+                    
+                    # Wait until parameters loaded
+                    wait = WebDriverWait(self.driver, 10)
+                    wait.until(done_loading)
+
+                    submit = self.driver.find_element_by_id('btnSubmit')
+                    submit.click()
+
+                    s = BeautifulSoup(self.driver.page_source)
+                    print s.prettify()
+
+                    self.driver.save_screenshot("./screenshot.png")
 
         self.driver.quit()
 
